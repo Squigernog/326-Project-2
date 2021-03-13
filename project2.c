62fbaf1bf47562ce45f2662be9c573a6c17d604c
Luke <LLolsen97@yahoo.com>
2021-03-12 19:08:15 -0800

    Start of project

---
+++ project2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+/**
+* CECS 326 Project 2
+* The purpose of this program is to use 2 pthreads to sort one array in two parts and merge those two arrays into one sorted array.
+* @author Luke Olsen
+* @author Nicholas Wong
+* @version 0.1
+* @date 3/14/2021
+*/
+
+int main(int argc, const char* argv[])
+{
+
+}
+
+void *sorter(int arr[])
+{
+    
+}
+
+void *merger(int arr1[], int arr2[])
+{
+
+}
