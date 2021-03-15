7c41a9a9afb12543c5c6af9a54b62b0944ae697a
NicholasW <nwong99729544@gmail.com>
2021-03-14 17:13:02 -0700

    Update project2.c
    this is all i got so far. I still have to allocate memory to it and do some more stuff with the pthreads

--- project2.c
+++ project2.c
@@ -11,10 +11,47 @@
  * @version 0.1
  * @date 3/14/2021
  */
+ 
+ //List for testing
+int list[10]={67,3,24,5,2,99,432,77,45,1};
 
+ //2 for the sorting, and 1 for merging.
+ #define NUM_THREADS 3
+
+ 
 int main(int argc, const char* argv[])
 {
-
+    //time how long it takes.
+    clock_t t1, t2; // Time the both thread 1 and 2.
+    //determine the size of the list.
+    int size = sizeof(list)/sizeof(int);
+    int arrsize1;
+    int arrsize2;
+    
+    
+    //check if the array size is even.
+    //if not then add 1 more to the first array.
+    if(size%2 == 0){
+   	 arrsize1=size/2;
+   	 arrsize2=size/2;
+    }
+    else{
+   	 arrsize1=(size/2) +!;
+   	 arrsize2=size/2
+    }
+    pthread_t thread[NUM_THREADS];
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    
+    //Create the threads for sorting.
+    pthread_create(&thread[0], &attr, countSort,(void*)NULL);
+    pthread_create(&thread[1], &attr, countSort,(void*)NULL);
+    
+    //Wait for the threads to finish.
+    pthread_join(thread[0],NULL);
+    pthread_join(thread[1],NULL);
+    
+    
 }
 
 /**
@@ -24,11 +61,12 @@ int main(int argc, const char* argv[])
  */
 void *sorter(int *arr[], int n)
 {
-    //TODO implement pthreads
-    int m = getMax(arr, n);  // find greatest value in array
 
-    for(int exp = 1; m / exp > 0; exp *= 10)
-        countSort(arr, n, exp);
+	//TODO implement pthreads
+	int m = getMax(arr, n);  // find greatest value in array
+
+	for(int exp = 1; m / exp > 0; exp *= 10)
+    	countSort(arr, n, exp);
 }
 
 /**
@@ -39,11 +77,11 @@ void *sorter(int *arr[], int n)
  */
 int getMax(int arr[], int n)
 {
-    int max = arr[0];
-    for(int i = 1; i < n; i++)
-        if(arr[i] > max)
-            max = arr[i];
-    return max;
+	int max = arr[0];
+	for(int i = 1; i < n; i++)
+    	if(arr[i] > max)
+        	max = arr[i];
+	return max;
 }
 
 /**
@@ -54,25 +92,29 @@ int getMax(int arr[], int n)
  */
 void countSort(int *arr[], int n, int exp)
 {
-    int output[n];  // output array
-    int i, count[10] = {0};
+	int output[n];  // output array
+	int i, count[10] = {0};
 
-    for(i = 0; i < n; i++)
-        count[(*arr[i] / exp) % 10]++;
+	for(i = 0; i < n; i++)
+   	 count[(*arr[i] / exp) % 10]++;
 
-    for(i = 1; i < 10; i++)
-        count[i] += count[i-1];
+   	 for(i = 1; i < 10; i++)
+   		 count[i] += count[i-1];
 
-    for(i = n - 1; i >= 0; i--)
+   	 for(i = n - 1; i >= 0; i--)
     {
-        output[count[(*arr[i] / exp) % 10] - 1] = arr[i];
-        count[(*arr[i] / exp) % 10]--;
-    }
+    	output[count[(*arr[i] / exp) % 10] - 1] = arr[i];
+    count[(*arr[i] / exp) % 10]--;
+   	 }
 
-    // copy from output to arr
-    for(i = 0; i < n; i++)
-        *arr[i] = output[i];
+   	 // copy from output to arr
+  	 for(i = 0; i < n; i++)
+  	  	*arr[i] = output[i];
 }
+
+/**
+* Merge Thread
+*/
 
 void *merger(int arr1[], int arr2[])
 {
